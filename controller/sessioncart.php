ce59d241f94878f516442c9c974d37cebd746a28
littlehi <j040200@gmail.com>
2020-04-29 23:04:36 +0800

    增加寫入session的判斷（未完

--- controller/sessioncart.php
+++ controller/sessioncart.php
@@ -5,11 +5,12 @@
     $size = $_POST['size'];
     $period = $_POST['period'];
     $others = $_POST['others'];
+    $num = 1;
 
-    $proarray = array($pid , $size , $period , $others);
+    $proarray = array($pid , $size , $period , $others , $num);
     $_SESSION['orderlist'] = $proarray;
 
-    if($proarray != null) {
+    if($proarray != null && $pid = $proarray[0][$pid]) {
         echo '已加入購物車';
         header( 'Location:' . "../view/programinside.php?id=".$pid);
     }else{
